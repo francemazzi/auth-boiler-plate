b1072e8ca032aadf0d15d511d545fa0d179ef7be
FraSma <francemazzi@gmail.com>
2025-01-21 03:47:28 +0100

    feat: postgress and redis errors

--- bin/create-express-auth.js
+++ bin/create-express-auth.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { execSync } from 'child_process';
-import path from 'path';
-import fs from 'fs';
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
 
 const colors = {
   reset: '\x1b[0m',
